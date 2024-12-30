9a96be1e45eaf28d53eae58ec0952f56d8c7a0e8
tlswlsdntks <tlswlsdntks@naver.com>
2024-12-30 17:14:13 +0900

    baekjoon

--- src/level2/level2_5/Main.java
+++ src/level2/level2_5/Main.java
@@ -1,7 +1,25 @@
 package level2.level2_5;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long h, m;
+        do {
+            h = scanner.nextLong();
+            m = scanner.nextLong();
+        } while ((0 > h || h > 23) || (0 > m || m > 59));
 
+        m -= 45;
+        if (m < 0) {
+            h -= 1;
+            m += 60;
+        }
+        if (h < 0) {
+            h += 24;
+        }
+        System.out.println(h);
+        System.out.println(m);
     }
 }
---
+++ src/level2/level2_6/Main.java
@@ -0,0 +1,7 @@
+package level2.level2_6;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
