8ba98b83b1011fee053134acdceccf59960439e7
tlswlsdntks <tlswlsdntks@naver.com>
2024-12-31 11:34:08 +0900

    baekjoon

--- src/level2/level2_7/Main.java
+++ src/level2/level2_7/Main.java
@@ -1,40 +1,19 @@
 package level2.level2_7;
 
-import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        long count = 0, target = 0, max = 0;
-        for (long i = 0; i < getDices().length; i++) {
-            if (max < i) {
-                max = i;
-            }
-            if (target == i) {
-                count++;
-            }
-            target = i;
-        }
-        getPrice(count, target, max);
-    }
-
-    private static long[] getDices() {
-        return new long[]{new Random().nextLong(6) + 1, new Random().nextLong(6) + 1, new Random().nextLong(6) + 1};
-    }
-
-    private static void getPrice(long count, long target, long max) {
-        System.out.println("count = " + count);
-        System.out.println("target = " + target);
-        System.out.println("max = " + max);
-        switch ((int) count) {
-            case 3:
-                System.out.println(10_000 + (target) * 1_000);
-                break;
-            case 2:
-                System.out.println(1_000 + (target) * 100);
-                break;
-            case 1:
-                System.out.println(max * 100);
-                break;
+        Scanner scanner = new Scanner(System.in);
+        long[] dices = {scanner.nextLong(), scanner.nextLong(), scanner.nextLong()};
+        if (dices[0] == dices[1] && dices[1] == dices[2]) {
+            System.out.println(10_000 + (dices[1]) * 1_000);
+        } else if (dices[0] == dices[1] || dices[0] == dices[2]) {
+            System.out.println(1_000 + (dices[0]) * 100);
+        } else if (dices[1] == dices[2]) {
+            System.out.println(1_000 + (dices[1]) * 100);
+        } else {
+            System.out.println(Math.max(Math.max(dices[0], dices[1]), dices[2]) * 100);
         }
     }
 }
